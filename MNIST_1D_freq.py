8d2d507a10494459b97d0c8030e293ef4f6e9929
Oliver Potocki <31374948+OliverPotocki@users.noreply.github.com>
2022-02-05 22:15:52 +0100

    Version presented on 30.01.2022

---
+++ MNIST_1D_freq.py
@@ -0,0 +1,167 @@
+import numpy as np
+import torch
+import random
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter
+from scipy.interpolate import interp1d
+from scipy.fftpack import dct, idct
+
+class ObjectView(object):
+    def __init__(self, d): self.__dict__ = d
+
+def plot_signals(xs, t, labels=None, args=None, title=None, ratio=2.6, do_transform=False, dark_mode=False, zoom=1):
+    rows, cols = 1, 10
+    fig = plt.figure(figsize=[cols*1.5,rows*1.5*ratio], dpi=60)
+    for r in range(rows):
+        for c in range(cols):
+            ix = r*cols + c
+            x, t = xs[ix], t
+            ax = plt.subplot(rows,cols,ix+1)
+
+            # plot the data
+            if do_transform:
+                assert args is not None, "Need an args object in order to do transforms"
+                x, t = transform(x, t, args)  # optionally, transform the signal in some manner
+            if dark_mode:
+                plt.plot(x, t, 'wo', linewidth=6)
+                ax.set_facecolor('k')
+            else:
+                plt.plot(x, t, 'k-', linewidth=2)
+            if labels is not None:
+                plt.title("label=" + str(labels[ix]), fontsize=22)
+            plt.xlim(-zoom,zoom) ; plt.ylim(-zoom,zoom)
+            plt.gca().invert_yaxis() ; plt.xticks([], []), plt.yticks([], [])
+    if title is None:
+        fig.suptitle('Noise free', fontsize=24, y=1.1)
+    else:
+        fig.suptitle(title, fontsize=24, y=1.1)
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.tight_layout() ; plt.show()
+
+def get_templates():
+    d0 = np.asarray([5,6,6.5,6.75,7,7,7,7,6.75,6.5,6,5])
+    d1 = np.asarray([5,3,3,3.4,3.8,4.2,4.6,5,5.4,5.8,5,5])
+    d2 = np.asarray([5,6,6.5,6.5,6,5.25,4.75,4,3.5,3.5,4,5])
+    d3 = np.asarray([5,6,6.5,6.5,6,5,5,6,6.5,6.5,6,5])
+    d4 = np.asarray([5,4.4,3.8,3.2,2.6,2.6,5,5,5,5,5,5])
+    d5 = np.asarray([5,3,3,3,3,5,6,6.5,6.5,6,4.5,5])
+    d6 = np.asarray([5,4,3.5,3.25,3,3,3,3,3.25,3.5,4,5])
+    d7 = np.asarray([5,7,7,6.6,6.2,5.8,5.4,5,4.6,4.2,5,5])
+    d8 = np.asarray([5,4,3.5,3.5,4,5,5,4,3.5,3.5,4,5])
+    d9 = np.asarray([5,4,3.5,3.5,4,5,5,5,5,4.7,4.3,5])
+    
+    x = np.stack([d0,d1,d2,d3,d4,d5,d6,d7,d8,d9])
+    x -= x.mean(1,keepdims=True) # whiten
+    x /= x.std(1,keepdims=True)
+    x -= x[:,:1]  # signal starts and ends at 0
+    
+    templates = {'x': x/6., 't': np.linspace(-5, 5, len(d0))/6.,
+                 'y': np.asarray([0,1,2,3,4,5,6,7,8,9])}
+    return templates
+    
+# transformations of the templates which will make them harder to fit
+def pad(x, padding, noise=(0,0)):
+    low, high = padding
+    pn, sn = noise
+    x = dct(x, norm="ortho")
+    p = low + int(np.random.rand()*(high-low+1))
+    y = np.zeros((p)) + np.random.normal(loc=0.0, scale=pn, size=(p,))
+    x = x + np.random.normal(loc=0.0, scale=sn, size=np.shape(x))
+    return idct(np.concatenate([x, y]), norm="ortho")
+
+def shear(x, scale=10):
+    coeff = scale*(np.random.rand() - 0.5)
+    return x - coeff*np.linspace(-0.5,.5,len(x))
+
+def translate(x, max_translation):
+    k = np.random.choice(max_translation)
+    return np.concatenate([x[-k:], x[:-k]])
+
+def corr_noise_like(x, scale):
+    noise = scale * np.random.randn(*x.shape)
+    return gaussian_filter(noise, 2)
+
+def interpolate(x, N):
+    scale = np.linspace(0,1,len(x))
+    new_scale = np.linspace(0,1,N)
+    new_x = interp1d(scale, x, axis=0, kind='linear')(new_scale)
+    return new_x
+
+def transform(x, y, args, eps=1e-8):
+    new_x = pad(x+eps, args.padding, args.noise) # pad
+    new_x = interpolate(new_x, args.template_len + args.padding[-1])  # dilate
+    new_y = interpolate(y, args.template_len + args.padding[-1])
+    new_x *= (1 + args.scale_coeff*(np.random.rand() - 0.5))  # scale
+    #new_x = translate(new_x, args.max_translation)  #translate
+    
+    # add noise
+    mask = new_x != 0
+    new_x = mask*new_x + (1-mask)*corr_noise_like(new_x, args.corr_noise_scale)
+    
+    # shear and interpolate
+    new_x = shear(new_x, args.shear_scale)
+    new_x = interpolate(new_x, args.final_seq_length) # subsample
+    new_x = new_x
+    new_y = interpolate(new_y, args.final_seq_length)
+    return new_x, new_y
+
+def get_dataset_args(noise=(0,0), as_dict=False):
+    arg_dict = {'num_samples': 5000,
+            'train_split': 0.8,
+            'template_len': 12,
+            'padding': [36,60],
+            'scale_coeff': .4, 
+            'max_translation': 42,
+            'noise': noise,
+            'corr_noise_scale': 0.0,
+            'shear_scale': 0.0,
+            'shuffle_seq': False,
+            'final_seq_length': 40,
+            'seed': 42}
+    return arg_dict if as_dict else ObjectView(arg_dict)
+
+def make_dataset(args=None, template=None,):
+    templates = get_templates() if template is None else template
+    args = get_dataset_args() if args is None else args
+    np.random.seed(args.seed) # reproducibility
+    
+    xs, ys = [], []
+    samples_per_class = args.num_samples // len(templates['y'])
+    for label_ix in range(len(templates['y'])):
+        for example_ix in range(samples_per_class):
+            x = templates['x'][label_ix]
+            t = templates['t']
+            y = templates['y'][label_ix]
+            x, new_t = transform(x, t, args) # new_t transformation is same each time
+            xs.append(x) ; ys.append(y)
+    
+    batch_shuffle = np.random.permutation(len(ys)) # shuffle batch dimension
+    xs = np.stack(xs)[batch_shuffle]
+    ys = np.stack(ys)[batch_shuffle]
+    
+    if args.shuffle_seq: # maybe shuffle the spatial dimension
+        seq_shuffle = np.random.permutation(args.final_seq_length)
+        xs = xs[...,seq_shuffle]
+    
+    new_t = new_t/xs.std()
+    xs = (xs-xs.mean())/xs.std() # center the dataset & set standard deviation to 1
+
+    # train / test split
+    split_ix = int(len(ys)*args.train_split)
+    dataset = {'x': xs[:split_ix], 'x_test': xs[split_ix:],
+               'y': ys[:split_ix], 'y_test': ys[split_ix:],
+               't':new_t, 'templates': templates}
+    return dataset
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def gen_MNIST_1D_freq(noise):
+    args = get_dataset_args(noise)
+    set_seed(args.seed)
+    args.shuffle_seq = False
+    data = make_dataset(args=args,)  # make the dataset
+    return data
